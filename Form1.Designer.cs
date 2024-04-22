f4cd3b4f0194afb36be7996c6e2ea0e9aea15be8
lautaroquiligotti <lautaroquiligotti@LAUTAROQUIL24F4>
2024-04-22 18:19:12 -0300

    Firma 22abril

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(497, 403);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(75, 23);
+            this.btnGrabar.TabIndex = 0;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(302, 403);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(77, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(654, 360);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // frmFirma
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGrabar);
             this.Name = "frmFirma";
             this.Text = "Firma";
+            this.Load += new System.EventHandler(this.frmFirma_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,57 @@ namespace pryLautaroQuiligotti
 {
     public partial class frmFirma : Form
     {
+        private Bitmap ArchivoImagen;
+        private string carpetaFirmas;
+
         public frmFirma()
         {
             InitializeComponent();
+            ArchivoImagen = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            carpetaFirmas = "firmas";
+            if (!Directory.Exists(carpetaFirmas))
+            {
+                Directory.CreateDirectory(carpetaFirmas);
+            }
+        }
+
+        private void frmFirma_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics objetoLoco = Graphics.FromImage(ArchivoImagen))
+                {
+                    objetoLoco.FillEllipse(Brushes.Black, e.X, e.Y, 5, 5);
+                }
+                pictureBox1.Image = ArchivoImagen;
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            ArchivoImagen = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = ArchivoImagen;
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            string FechaActual = DateTime.Now.ToString("(yyyy.MM.dd) HH.mm");
+            string nombreArchivo = $"{FechaActual}.png"; // Cambiado a PNG
+            string Guardar = Path.Combine(carpetaFirmas, nombreArchivo);
+
+            // Guardar como PNG
+            ArchivoImagen.Save(Guardar, System.Drawing.Imaging.ImageFormat.Png);
+            MessageBox.Show("FIRMA GUARDADA CON EXITO");
         }
     }
 }
